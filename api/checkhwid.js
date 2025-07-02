67d96052e3a1ca23b756be5f8cf3ed979006f9d2
yz188u <sumiratcahyapermana@gmail.com>
2025-07-03 02:51:53 +0700

    Update checkhwid.js

--- api/checkhwid.js
+++ api/checkhwid.js
@@ -10,7 +10,6 @@ export default async function handler(req, res) {
       return res.status(400).json({ valid: false, error: 'HWID is missing' });
     }
 
-    // Contoh: validasi dari array manual
     const validHWIDs = ["5eaa7570-129a-47f9-8166-c8dcd002482c"];
     const isValid = validHWIDs.includes(hwid);
 
@@ -20,3 +19,11 @@ export default async function handler(req, res) {
     return res.status(500).json({ valid: false, error: 'Internal Server Error' });
   }
 }
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '1mb',
+    },
+  },
+};
