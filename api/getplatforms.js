3050ef690c4b9f0060c1d1057b8a52bfc41375c4
yz188u <sumiratcahyapermana@gmail.com>
2025-07-03 17:18:28 +0700

    Create getplatforms.js

---
+++ api/getplatforms.js
@@ -0,0 +1,12 @@
+import fetch from "node-fetch";
+
+export default async function handler(_, res) {
+  try {
+    const platformURL = `https://xzuyaxhubkey-default-rtdb.asia-southeast1.firebasedatabase.app/platforms.json`;
+    const fbRes = await fetch(platformURL);
+    const data = await fbRes.json();
+    return res.status(200).json(data || {});
+  } catch (err) {
+    return res.status(500).json({ error: "Failed to load platforms" });
+  }
+}
